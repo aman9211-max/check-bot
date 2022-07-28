4cb4ba837f0449a9674bb1c7dad706642edf212c
Aman kumar singh <kumarsinghaman3500@gmail.com>
2022-07-28 15:49:34 +0530

    commit

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 points = [];
+var count = 0;
+let values = document.getElementById("values");
 mouseMove = function (event) {
-    var eventDoc, doc, body;
-        event = event || window.event; // IE-ism
-        if (event.pageX != null && event.clientX != null) {
-            eventDoc = (event.target && event.target.ownerDocument) || document;
-            doc = eventDoc.documentElement;
-            body = eventDoc.body;
+  var eventDoc, doc, body;
+  event = event || window.event; // IE-ism
+  if (event.pageX != null && event.clientX != null) {
+    eventDoc = (event.target && event.target.ownerDocument) || document;
+    doc = eventDoc.documentElement;
+    body = eventDoc.body;
 
-            event.pageX = event.clientX +
-              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-              (doc && doc.clientLeft || body && body.clientLeft || 0);
-            event.pageY = event.clientY +
-              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+    event.pageX =
+      event.clientX +
+      ((doc && doc.scrollLeft) || (body && body.scrollLeft) || 0) -
+      ((doc && doc.clientLeft) || (body && body.clientLeft) || 0);
+    event.pageY =
+      event.clientY +
+      ((doc && doc.scrollTop) || (body && body.scrollTop) || 0) -
+      ((doc && doc.clientTop) || (body && body.clientTop) || 0);
 
-              let obj = [event.pageX, event.pageY];
-              points.push(obj);
-        }
-        // console.log(points)
-        console.log(checkIsLine());
-}
-  document.onmousemove = mouseMove;
-  console.log(checkIsLine());
-
+    let obj = [event.pageX, event.pageY];
+    points.push(obj);
+  }
+  // console.log(points)
+    if(count == 0) {
+      let p = checkIsLine();
+      var z = document.createElement("li"); // is a node
+      z.innerHTML = `${p}`;
+      values.appendChild(z);
+      console.log(checkIsLine());
+    }
+    count++;
+    count %= 100;
+};
+document.onmousemove = mouseMove;
+console.log(checkIsLine());
